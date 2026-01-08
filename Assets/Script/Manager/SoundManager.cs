b532fad52c3023d8c434492150380aa7bccb2a12
23cu0235 <160844409+23cu0235@users.noreply.github.com>
2026-01-08 12:33:24 +0900

    SE

--- Assets/Script/Manager/SoundManager.cs
+++ Assets/Script/Manager/SoundManager.cs
@@ -1,11 +1,67 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class seDeta
+{
+    public string seName;       //SE名
+    public AudioClip seClip;    //SE音
+}
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private seDeta[] seDatas;
+
+    private Dictionary<string, AudioClip> seDictionary;
+    private AudioSource audioSource;
+
+    //シングルトン
+    public static SoundManager instance;
+
+    void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) Debug.Log("audioSourceの取得に失敗");
+
+        seDictionary = new Dictionary<string, AudioClip>();
+        foreach(var se in seDatas)
+        {
+            if(!seDictionary.ContainsKey(se.seName))
+            {
+                seDictionary.Add(se.seName,se.seClip);
+            }
+            else
+            {
+                Debug.Log("SEが重複");
+            }
+        }
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //指定したSEを再生する
+    public void PlaySE(string seName)
+    {
+        if (audioSource == null) Debug.Log("audioSourceがnull");
+
+        //SE名に対応するAudioClipを取得
+        if(seDictionary.TryGetValue(seName, out AudioClip clip))
+        {
+            //再生
+            audioSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.Log("SE not found");
+        }
     }
 
     // Update is called once per frame
